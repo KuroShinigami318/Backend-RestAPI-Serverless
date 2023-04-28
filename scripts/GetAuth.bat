570a6d9b6415abdf27520df0082bf3f9c35e581a
KuroShinigami318 <53936920+KuroShinigami318@users.noreply.github.com>
2023-04-28 08:54:45 +0000

    Add scripts for testing functions

---
+++ scripts/GetAuth.bat
@@ -0,0 +1,1 @@
+curl --url https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBDPRtq6-RLD713Im-BXwj4Fnz0O0A_Pkw -H "Content-Type: application/json" --data-binary "{\"returnSecureToken\":true}" -o auth.json
---
+++ scripts/Test.py
@@ -0,0 +1,81 @@
+import requests
+import json
+import subprocess
+import datetime
+import os
+
+backend_url = "http://127.0.0.1:5001/backend-restapi-5ee97/us-central1/app"
+
+def GetAuth(filePath):
+    data = ReadFromJSON(filePath)
+    now = datetime.datetime.now()
+    if (not data):
+        scriptLocation = os.getcwd() + '/GetAuth.bat'
+        r = subprocess.call([scriptLocation])
+        if (r != 0):
+           print('Error while execute GetAuth batch script')
+           return False
+        UpdateExpireDate(filePath=filePath, updateTime=now)
+        return True
+    
+    if ('expiresAt' not in data or datetime.datetime.fromisoformat(data['expiresAt']) < now):  
+        header = {"Content-Type": "application/json"}
+        body = {
+        "grant_type": "refresh_token",
+        "refresh_token": data['refreshToken']
+        }
+        body = json.dumps(body)
+        r = requests.post(url='https://securetoken.googleapis.com/v1/token?key=AIzaSyBDPRtq6-RLD713Im-BXwj4Fnz0O0A_Pkw', headers=header, data=body)
+        result = json.loads(r.text)
+        if('error' in result):
+           print('response with error: %s\nMessage: %s'%(result['error']['status'], result['error']['message']))
+           return False
+        data['idToken'] = result['id_token']
+        data['refreshToken'] = result ['refresh_token']
+        data['expiresIn'] = result['expires_in']
+        UpdateExpireDate(data=data, updateTime=now)
+    return True
+
+def UpdateExpireDate(updateTime, filePath = None, data = None):
+    if (not filePath is None):
+        data = ReadFromJSON(filePath)
+    deltaTime = datetime.timedelta(seconds=float(data['expiresIn']))
+    data['expiresAt'] = datetime.datetime.isoformat(updateTime + deltaTime)
+    jsonData = json.dumps(data)
+    WriteToJSON('auth.json', jsonData)
+
+def Login(url, idToken, idUser, pwd):
+    startTime = datetime.datetime.now()
+    header = {"Authorization": "Bearer " + idToken}
+    data = {"id": idUser, "pass": pwd}
+    r = requests.post(url=url, headers=header, data=data)
+    finishTime = datetime.datetime.now()
+    elapsed = finishTime - startTime
+    print(r.text)
+    print('Execution finished with %ds'%(elapsed.seconds))
+
+def ReadFromJSON(filePath):
+    if (not os.path.isfile(filePath)):
+       return False
+    f = open(filePath)
+    data = json.load(f)
+    return data
+
+def WriteToJSON(filePath, data):
+    if (not IsJSON(data)):
+       print("data is not in json format")
+       return
+    f = open(filePath, 'w')
+    f.write(data)
+
+def IsJSON(jsonData):
+  try:
+    json.loads(jsonData)
+  except ValueError as e:
+    return False
+  return True
+
+curDir = os.getcwd()
+if (GetAuth(curDir + '/auth.json')):
+    data = ReadFromJSON(curDir + '/auth.json')
+    Login(backend_url + "/login", data['idToken'], "3118412003", "")
